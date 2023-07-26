1ab2ab12cb9091474b9cd2e71ea3e622a0b2cb02
Saurabh  Maithani <saurabhmaithani01921@gmail.com>
2023-07-26 15:28:16 +0530

    make sevice for send message

---
+++ src/main/java/com/rabbitmq/publisher/RabbitMQPublisher.java
@@ -0,0 +1,25 @@
+package com.rabbitmq.publisher;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import org.slf4j.Logger;
+
+@Service
+public class RabbitMQPublisher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQPublisher.class);
+    @Value("${rabbitmq.exchange.name}")
+    public String exchangeName;
+    @Value("${rabbitmq.routing.key}")
+    public String routingKey;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        LOGGER.info("Message Send -> %s" + message);
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+    }
+}
